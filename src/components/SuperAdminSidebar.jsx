60261ebd1d6d31bdc44ecfae8db1ed4290b91e5f
Muhammad Abdullah <abdullahfalak007@gmail.com>
2024-07-11 17:59:10 +0500

    Voice Assistant Page done

--- src/components/SuperAdminSidebar.jsx
+++ src/components/SuperAdminSidebar.jsx
@@ -23,7 +23,7 @@ const SuperAdminSidebar = ({ setSelectedPage }) => {
       <img
         src={imagesPath.Signup.logo}
         alt="Logo"
-        className="w-46 h-18 mb-4 mx-4"
+        className="w-46 h-18 mb-4 ml-[-2rem] mt-[-2.2rem]"
       />
       <ul className="mt-[3rem]">
         {sidebarMenuItems.map((item, index) => {
@@ -31,7 +31,7 @@ const SuperAdminSidebar = ({ setSelectedPage }) => {
           return (
             <li
               key={index}
-              className={`flex items-center p-2 cursor-pointer p-4 ${
+              className={`flex items-center p-2 cursor-pointer px-8 my-8 ${
                 isSelected ? "bg-white text-black" : "hover:bg-blue-700"
               }`}
               onClick={() => handleMenuItemClick(index, item.text)}
--- src/components/UserSidebar.jsx
+++ src/components/UserSidebar.jsx
@@ -29,15 +29,15 @@ const UserSidebar = ({ setSelectedPage }) => {
       <img
         src={imagesPath.Signup.logo}
         alt="Logo"
-        className="w-46 h-18 mb-4 mx-4"
+        className="w-46 h-18 mb-4 ml-[-2rem] mt-[-2.2rem]"
       />
-      <ul className="mt-[3rem]">
+      <ul className="">
         {userSidebarMenuItems.map((item, index) => {
           const isSelected = selectedItem === index;
           return (
             <li
               key={index}
-              className={`flex items-center p-2 cursor-pointer p-4 ${
+              className={`flex items-center p-2 cursor-pointer px-8 my-8 ${
                 isSelected ? "bg-white text-black" : "hover:bg-blue-700"
               }`}
               onClick={() => handleMenuItemClick(index, item.text)}

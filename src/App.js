e215da23ae5c246e78c01eb7337a78f694d59083
Muhammad Abdullah <abdullahfalak007@gmail.com>
2024-07-16 07:14:27 +0500

    Just pixel perfection remaining

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import React, { useState } from "react";
-// import SuperAdminSidebar from "./components/SuperAdminSidebar";
-// import UserSidebar from "./components/UserSidebar";
-// import VirtualAssistantsPage from "./pages/VirtualAssistantsPage";
-// import VoiceAssistantPage from "./pages/VoiceAssistantPage";
-
-// const App = () => {
-//   const [selectedPage, setSelectedPage] = useState("Dashboard");
-//   const isSuperAdmin = true; // Change this to toggle between super admin and user
-
-//   return (
-//     <div className="flex h-screen">
-//       {isSuperAdmin ? (
-//         <SuperAdminSidebar setSelectedPage={setSelectedPage} />
-//       ) : (
-//         <UserSidebar setSelectedPage={setSelectedPage} />
-//       )}
-//       {selectedPage === "Virtual Assistants" && <VirtualAssistantsPage />}
-//       {selectedPage === "Voice Assistant" && <VoiceAssistantPage />}
-//       {/* Add other page conditions as needed */}
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 
 import VoiceAssistantPage from "./pages/VoiceAssistantPage";
--- src/pages/KnowledgeBasePageForAdmin.jsx
+++ src/pages/KnowledgeBasePageForAdmin.jsx
@@ -1,109 +1,3 @@
-// import React, { useState } from "react";
-// import AdminSidebar from "../components/AdminSidebar";
-// import Navbar from "../components/Navbar";
-// import KnowledgeBaseCard from "../components/KnowledgeBaseCard";
-// import KnowledgeBaseModal from "../components/KnowledgeBaseModal";
-// import knowledgeBaseData from "../data/knowledgeBaseData.json"; // Sample data
-// import clientsData from "../data/clientsData.json"; // Clients data
-
-// const KnowledgeBasePageForAdmin = () => {
-//   const [data, setData] = useState(knowledgeBaseData);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [formData, setFormData] = useState({
-//     id: null,
-//     client: "",
-//     bot: "",
-//     dateCreated: "",
-//     documents: [],
-//   });
-//   const [isEditMode, setIsEditMode] = useState(false);
-
-//   const openModal = (item = null) => {
-//     if (item) {
-//       setFormData(item);
-//       setIsEditMode(true);
-//     } else {
-//       setFormData({
-//         id: null,
-//         client: "",
-//         bot: "",
-//         dateCreated: "",
-//         documents: [],
-//       });
-//       setIsEditMode(false);
-//     }
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   const saveItem = () => {
-//     if (isEditMode) {
-//       // Update existing entry
-//       const updatedData = data.map((entry) =>
-//         entry.id === formData.id ? formData : entry
-//       );
-//       setData(updatedData);
-//     } else {
-//       // Add new entry
-//       const newEntry = {
-//         ...formData,
-//         id: data.length + 1,
-//         dateCreated: new Date().toLocaleDateString(),
-//       };
-//       setData([newEntry, ...data]);
-//     }
-//     closeModal();
-//   };
-
-//   const deleteItem = (id) => {
-//     const updatedData = data.filter((item) => item.id !== id);
-//     setData(updatedData);
-//   };
-
-//   return (
-//     <div className="flex min-h-screen w-full">
-//       <AdminSidebar setSelectedPage={() => {}} className="h-full" />
-//       <div className="flex-1 flex flex-col">
-//         <div className="w-full">
-//           <Navbar
-//             title="Knowledge Base"
-//             showAddIcon={true}
-//             showSearchBar={true}
-//             searchBarPlaceholder="Search Company"
-//             showClientDropdown={true} // Pass the prop to show the client dropdown
-//             clients={clientsData} // Pass the clients data to the Navbar
-//             openAddModal={() => openModal()}
-//           />
-//         </div>
-//         <div className="flex-1 p-4 grid grid-cols-3 gap-4">
-//           {data.map((item) => (
-//             <KnowledgeBaseCard
-//               key={item.id}
-//               {...item}
-//               onEdit={openModal}
-//               onDelete={deleteItem}
-//             />
-//           ))}
-//         </div>
-//         <KnowledgeBaseModal
-//           isOpen={isModalOpen}
-//           onClose={closeModal}
-//           onSave={saveItem}
-//           formData={formData}
-//           setFormData={setFormData}
-//           isEditMode={isEditMode}
-//           clients={clientsData}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default KnowledgeBasePageForAdmin;
-
 import React, { useState } from "react";
 import AdminSidebar from "../components/AdminSidebar";
 import Navbar from "../components/Navbar";
@@ -153,7 +47,7 @@ const KnowledgeBasePageForAdmin = () => {
       newItem.dateCreated = new Date().toLocaleDateString(); // Set current date
       setData([...data, newItem]);
     }
-    closeModal();
+    // We no longer close the modal here to allow for training simulation
   };
 
   const deleteItem = (id) => {
